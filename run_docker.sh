eb83f1086f8cad64d2b0a085ebe29f386599891c
Trekkie Coder <trekkie@netlox.io>
2025-02-19 02:19:03 +0900

    updated docker script

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm --network host --privileged ghcr.io/loxilb-io/bw-alert:latest eth0 1 50 100 "172.17.0.3,192.168.1.100" "10.10.10.1"
+docker run --rm --network host --privileged --restart unless-stopped ghcr.io/loxilb-io/bw-alert:latest eth0 1 50 100 "172.17.0.3,192.168.1.100" "10.10.10.1"
 
